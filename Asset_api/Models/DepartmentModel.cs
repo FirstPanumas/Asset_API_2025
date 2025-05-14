040ca3db304a8c3c250f1d3e4473e177f623e358
FirstPanumas <107640002+FirstPanumas@users.noreply.github.com>
2025-05-14 23:24:40 +0700

    Add project files.

---
+++ Asset_api/Models/DepartmentModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Asset_api.Models
+{
+    public class DepartmentModel
+    {
+
+        public int Id { get; set; }
+        public string? Departmentname { get; set; }
+        public string? Shortname { get; set; }
+    }
+}
---
+++ Asset_api/SqlConnect.cs
@@ -0,0 +1,35 @@
+﻿using Npgsql;
+
+namespace Asset_api
+{
+    public class SqlConnect
+    {
+        public NpgsqlConnection GetConnection()
+        {
+            string host = "localhost";
+            string user = "postgres";
+            string password = "admin";
+            string db = "db_asset";
+            string port = "5432";
+
+
+            try
+            {
+
+                string strConn = String.Format("Host={1};Username={2};Password{3};Database={4};Post={5}", host, user, password, db, port);
+                using NpgsqlConnection conn = new NpgsqlConnection(strConn);
+                conn.Open();
+
+                return conn;
+
+
+            }
+            catch (Exception ex)
+            {
+
+                throw new Exception(ex.Message);
+            }
+
+        }
+    }
+}
